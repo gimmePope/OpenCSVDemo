990dc9545910e7379f37caacd97b1614a561d28e
Otulana Olujimi <jimiotulana@gmail.com>
2022-02-21 12:16:59 +0100

    Creating model class for CSV mapping

---
+++ src/main/java/com/pocosoft/csvImport/model/EmployeeDetails.java
@@ -0,0 +1,78 @@
+package com.pocosoft.csvImport.model;
+
+import javax.persistence.Entity;
+
+import com.opencsv.bean.CsvBindAndJoinByName;
+import com.opencsv.bean.CsvBindByName;
+
+
+@Entity
+public class EmployeeDetails {
+	
+	private long id;
+	@CsvBindByName
+	private String name;
+	@CsvBindByName
+	private String email;
+	@CsvBindByName
+	private String address;
+	@CsvBindByName
+	private String telephone;
+	
+	public EmployeeDetails() {
+		
+	}
+
+	public EmployeeDetails(String name, String email, String address, String telephone) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.address = address;
+		this.telephone = telephone;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	
+	
+	
+	
+
+}
