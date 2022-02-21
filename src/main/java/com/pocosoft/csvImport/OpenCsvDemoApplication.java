52c86a714c08b8562d7bb192aa5ac57d6ed92de3
Otulana Olujimi <jimiotulana@gmail.com>
2022-02-21 11:26:15 +0100

    Intital commit

---
+++ src/main/java/com/pocosoft/csvImport/OpenCsvDemoApplication.java
@@ -0,0 +1,13 @@
+package com.pocosoft.csvImport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenCsvDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenCsvDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pocosoft/csvImport/OpenCsvDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pocosoft.csvImport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenCsvDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
